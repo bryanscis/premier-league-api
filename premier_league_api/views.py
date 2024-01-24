e04c19d1ad04f49b3710f3a74d34f366fd4f9405
Bryan See <bryanscis@gmail.com>
2024-01-23 19:31:22 -0800

    Add functionality to bulk update fixtures

--- premier_league_api/views.py
+++ premier_league_api/views.py
@@ -43,23 +43,27 @@ def add_managers(request):
             Managers.objects.filter(team_name=manager_info[4]).update(first_name=manager_name[0], last_name=manager_name[1], age=manager_info[0], nationality= manager_info[5], team_name=manager_info[4], contract_expiry= manager_info[2], updated_at=timezone.now())
         return Response(status=status.HTTP_200_OK)
 
-@api_view(['POST'])
+@api_view(['POST', 'PUT'])
 @permission_classes([IsAuthenticated])
 def add_fixtures(request):
-
-    if request.method == 'POST':
-        response = requests.get('https://fixturedownload.com/feed/json/epl-2023').json()
-        for match in response:
-            match['HomeTeam'] = CONSTANTS.TEAMSFIXTURES[match['HomeTeam']]
-            match['AwayTeam'] = CONSTANTS.TEAMSFIXTURES[match['AwayTeam']]
-            match['DateUtc'] = match['DateUtc'][:-1] + '+00'
-            serializer = FixturesSerializer(data={'game_week': match['RoundNumber'], 'date_time': match['DateUtc'], 'stadium': match['Location'], 'home_team': match['HomeTeam'], 'away_team': match['AwayTeam'], 'home_score': match['HomeTeamScore'], 'away_score': match['AwayTeamScore'],'created_at': datetime.datetime.now(), 'updated_at': datetime.datetime.now()})
+    response = requests.get('https://fixturedownload.com/feed/json/epl-2023').json()
+    for match in response:
+        match['HomeTeam'] = CONSTANTS.TEAMSFIXTURES[match['HomeTeam']]
+        match['AwayTeam'] = CONSTANTS.TEAMSFIXTURES[match['AwayTeam']]
+        match['DateUtc'] = match['DateUtc'][:-1] + '+00'
+        if request.method == 'POST':
+            serializer = FixturesSerializer(data={'game_week': match['RoundNumber'], 'date_time': match['DateUtc'], 'stadium': match['Location'], 'home_team': match['HomeTeam'], 'away_team': match['AwayTeam'], 'home_score': match['HomeTeamScore'], 'away_score': match['AwayTeamScore'],'created_at': timezone.now(), 'updated_at': timezone.now()})
             try:
                 if serializer.is_valid(raise_exception=True):
                     serializer.save()
             except IntegrityError:
                 return Response(data={serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(status=status.HTTP_200_OK)
+        elif request.method == 'PUT':
+            try:
+                Fixtures.objects.filter(game_week=match['RoundNumber'], home_team=match['HomeTeam'], away_team=match['AwayTeam']).update(date_time= match['DateUtc'], home_score= match['HomeTeamScore'], away_score= match['AwayTeamScore'], updated_at=timezone.now())
+            except:
+                return Response(data={serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
