86f108258d10e0ef5814a6948e95cebee370e0f7
Bryan See <bryanscis@gmail.com>
2024-01-23 17:19:03 -0800

    Add functionality to bulk update Managers

--- premier_league_api/views.py
+++ premier_league_api/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse, HttpResponse
 from django.db import IntegrityError
+from django.utils import timezone
 from .models import Teams, Managers, Fixtures, Players
 from .serializers import TeamsSerializer, ManagersSerializer, FixturesSerializer, PlayersSerializer
 from rest_framework.decorators import api_view, permission_classes
@@ -11,29 +12,35 @@ import requests, json, datetime
 import premier_league_api.constants as CONSTANTS
 from bs4 import BeautifulSoup
 
-@api_view(['POST'])
+@api_view(['POST', 'PUT'])
 @permission_classes([IsAuthenticated])
 def add_managers(request):
+    response = requests.get("https://www.transfermarkt.us/premier-league/trainer/pokalwettbewerb/GB1", headers={"User-Agent":"Mozilla/5.0"})
+    soup = BeautifulSoup(response.content, 'html.parser').find('table', {'class':'items'}).find_all('tr')[1:]
+    all_managers = {}
+    # Split manager information
+    for manager in soup:
+        manager_name = manager.get_text(separator = "|").split('|')[1]
+        manager_nationality = manager.find( 'img', {'class': 'flaggenrahmen'})['title']
+        manager_team = manager.find( 'img', {'class': 'tiny_wappen'})['title']
+        all_managers[manager_name] = manager.get_text(separator = "|").split('|')[2:] + [ CONSTANTS.TEAMS[manager_team], manager_nationality]
     if request.method == 'POST':
-        response = requests.get("https://www.transfermarkt.us/premier-league/trainer/pokalwettbewerb/GB1", headers={"User-Agent":"Mozilla/5.0"})
-        soup = BeautifulSoup(response.content, 'html.parser').find('table', {'class':'items'}).find_all('tr')[1:]
-        all_managers = {}
-        # Split manager information
-        for manager in soup:
-            manager_name = manager.get_text(separator = "|").split('|')[1]
-            manager_nationality = manager.find( 'img', {'class': 'flaggenrahmen'})['title']
-            manager_team = manager.find( 'img', {'class': 'tiny_wappen'})['title']
-            all_managers[manager_name] = manager.get_text(separator = "|").split('|')[2:] + [ CONSTANTS.TEAMS[manager_team], manager_nationality]
         for manager, manager_info in all_managers.items():
             manager_info[2] = None if manager_info[2] == '?' else manager_info[2]
             manager_name = manager.split(" ", 1)
-            serializer = ManagersSerializer(data={'first_name': manager_name[0], 'last_name':manager_name[1], 'age':manager_info[0], 'nationality': manager_info[5], 'team_name':manager_info[4], 'contract_expiry': manager_info[2], 'created_at': datetime.datetime.now(), 'updated_at': datetime.datetime.now()})
+            serializer = ManagersSerializer(data={'first_name': manager_name[0], 'last_name':manager_name[1], 'age':manager_info[0], 'nationality': manager_info[5], 'team_name':manager_info[4], 'contract_expiry': manager_info[2], 'created_at': timezone.now(), 'updated_at': timezone.now()})
             # Save data if no errors
             try:
                 if serializer.is_valid(raise_exception=True):
                     serializer.save()
             except IntegrityError:
                 return Response(data={serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        for manager, manager_info in all_managers.items():
+            manager_info[2] = None if manager_info[2] == '?' else manager_info[2]
+            manager_name = manager.split(" ", 1)
+            Managers.objects.filter(team_name=manager_info[4]).update(first_name=manager_name[0], last_name=manager_name[1], age=manager_info[0], nationality= manager_info[5], team_name=manager_info[4], contract_expiry= manager_info[2], updated_at=timezone.now())
         return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST'])
